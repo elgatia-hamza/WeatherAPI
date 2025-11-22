cbe4c49229095f5c76f0771c9f2b2c4129bebc0b
Gatia <hamza.elgatia@usms.ac.ma>
2025-11-22 19:22:18 +0100

    feat: ajouter redis pour le caching

--- src/main/java/com/weather/weather_api/service/WeatherService.java
+++ src/main/java/com/weather/weather_api/service/WeatherService.java
@@ -1,10 +1,13 @@
 package com.weather.weather_api.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class WeatherService {
@@ -14,9 +17,23 @@ public class WeatherService {
 
     private final RestTemplate restTemplate = new RestTemplate();
 
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
     public Map<String, Object> getWeather(String city) {
+        Map<String, Object> cached = (Map<String, Object>) redisTemplate.opsForValue().get(city);
+
+        if (cached != null) {
+            return cached;
+        }
+
         String url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
                 + city + "?unitGroup=metric&key=" + apiKey + "&include=current";
-        return restTemplate.getForObject(url, Map.class);
+
+        Map<String, Object> response = restTemplate.getForObject(url, Map.class);
+
+        redisTemplate.opsForValue().set(city, response, 12, TimeUnit.HOURS);
+
+        return response;
     }
 }
